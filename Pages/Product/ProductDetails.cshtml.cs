9974afbdda95428c94212f3bb4e40baac72716d3
MathiasNoergaard <man010@edu.zealand.dk>
2024-04-12 13:14:28 +0200

    Added link to getAllProduct in productDetails

--- Pages/Product/ProductDetails.cshtml.cs
+++ Pages/Product/ProductDetails.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Blomsterbinderiet.Pages
+namespace Blomsterbinderiet.Pages.Product
 {
     public class ProductDetailsModel : PageModel
     {
