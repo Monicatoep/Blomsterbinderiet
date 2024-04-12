8c7a505b7f8db9d294b6dc5dde852b09dfa1201b
lausalber <150662642+lausalber@users.noreply.github.com>
2024-04-12 11:46:58 +0200

    MockData mappe og mockdata for både produkter og keywords er oprettet
    Model mappe og model klasser for Product og Keyword er oprettet
    
    Oprettet Product Mappe under Pages mappen
    
    Oprettet produkt side under GetAllProducts

---
+++ Models/Keyword.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blomsterbinderiet.Model
+{
+	public class Keyword
+	{
+        [Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+		public Keyword()
+		{
+		}
+
+		public Keyword(string name, string description, ICollection<Product> products)
+		{
+			Name = name;
+			Description = description;
+			Products = products;
+		}
+
+		public override string ToString()
+		{
+			return $"{{{nameof(ID)}={ID.ToString()}, {nameof(Name)}={Name}, {nameof(Description)}={Description}, {nameof(Products)}={Products}}}";
+		}
+	}
+}
