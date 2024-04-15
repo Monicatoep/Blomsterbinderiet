507bfe85ba2c3dd4f3a56071f4a0df7ae1fe2c4a
MathiasNoergaard <man010@edu.zealand.dk>
2024-04-15 10:24:44 +0200

    Jeg er begyndt lave en cookie baseret kurv

--- Pages/Product/ProductDetails.cshtml.cs
+++ Pages/Product/ProductDetails.cshtml.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace Blomsterbinderiet.Pages.Product
 {
@@ -9,6 +12,23 @@ namespace Blomsterbinderiet.Pages.Product
         public int Amount { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            var test = HttpContext.Request.Cookies;
+            foreach (var idk in HttpContext.Request.Cookies)
+            {
+                Console.WriteLine(idk);
+            }
+
+            var claims = new List<Claim> { 
+                new Claim("basketItem", "1") 
+            };
+            //claims.Add(new Claim(ClaimTypes.Role, temp.AccessLevel));
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            return RedirectToPage("/Basket/Basket");
         }
     }
 }
