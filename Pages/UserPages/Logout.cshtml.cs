4fc6716697eaccd3f9d62ea5ec754b57e744d9a4
MathiasNoergaard <man010@edu.zealand.dk>
2024-04-18 13:47:42 +0200

    Kurven bliver nu nulstillet når man logger ud

--- Pages/UserPages/Logout.cshtml.cs
+++ Pages/UserPages/Logout.cshtml.cs
@@ -2,14 +2,23 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Blomsterbinderiet.Service;
 
 namespace Blomsterbinderiet.Pages.Customer
 {
     public class LogoutModel : PageModel
     {
+        public BasketCookieService cookieService { get; set; }
+
+        public LogoutModel(BasketCookieService cookieService)
+        {
+            this.cookieService = cookieService;
+        }
+
         public async Task<IActionResult> OnGet()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            cookieService.SaveCookie(Response.Cookies, null);
             return RedirectToPage("/index");
         }
     }
