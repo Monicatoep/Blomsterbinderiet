96feecd5ccacc6f15602b3ddb5be7befe9c7b780
MathiasNoergaard <man010@edu.zealand.dk>
2024-04-19 10:40:39 +0200

    Tilføje default konstruktøren og ToString metoden til OrderLine klassen

--- Models/OrderLine.cs
+++ Models/OrderLine.cs
@@ -8,5 +8,14 @@
         public int ProductID { get; set; }
         public Product Product { get; set; }
         public int Amount { get; set; }
+
+        public OrderLine()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(ID)}={ID.ToString()}, {nameof(OrderID)}={OrderID.ToString()}, {nameof(Order)}={Order}, {nameof(ProductID)}={ProductID.ToString()}, {nameof(Product)}={Product}, {nameof(Amount)}={Amount.ToString()}}}";
+        }
     }
 }
