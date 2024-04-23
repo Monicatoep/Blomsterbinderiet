e6f8af76a49aae4a352cf7c9bb7df133d3ae5409
Monicatoep <69761812+Monicatoep@users.noreply.github.com>
2024-04-23 11:04:48 +0200

    Tilføjet design/funktionalitet til Index. Tilrettet ProductService med Liste af Products.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blomsterbinderiet.Service;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Blomsterbinderiet.Pages
 {
     public class IndexModel : PageModel
     {
+        
         private readonly ILogger<IndexModel> _logger;
+        public ProductService ProductService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ProductService productService)
         {
+            ProductService = productService;
             _logger = logger;
         }
 
