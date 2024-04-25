1d6e8173428687ee55addc43fe7ea398ed5fd8c4
FrederikkeL <fretzo@hotmail.com>
2024-04-25 13:41:22 +0200

    lavet en CreateProduct side

---
+++ Pages/Admin/CreateProduct.cshtml.cs
@@ -0,0 +1,62 @@
+using Blomsterbinderiet.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blomsterbinderiet.Pages.Admin
+{
+    public class CreateProductModel : PageModel
+    {
+        [BindProperty]
+        public Models.Product Product { get; set; }
+        public string Confirmation { get; set; }
+        public ProductService ProductService { get; set; }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public CreateProductModel(ProductService productService, IWebHostEnvironment webHostEnvironment)
+        {
+            ProductService = productService;
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public void OnGet()
+        {
+           
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                Confirmation = "Oprettelse fejlede";
+                return Page();
+            }
+            Models.Product Product = new Models.Product();
+            
+            if (Product.UploadedImage != null)
+            {
+                Product.Image = ConvertToByteArray(Product.UploadedImage).Result;
+            }
+            //Product.Disabled = InputProduct.Disabled;
+            await ProductService.AddProductAsync(Product);
+
+            Confirmation = "Tilføjet produktet";
+            return Page();
+            //return RedirectToPage("/Product/GetAllProducts");
+        }
+
+        public async Task<byte[]> ConvertToByteArray(IFormFile temp)
+        {
+            var filePath = Path.Combine(Path.Combine(WebHostEnvironment.WebRootPath, "images"), temp.FileName);
+
+            using (FileStream fs = System.IO.File.Create(filePath))
+            {
+                temp.CopyTo(fs);
+            }
+            byte[] temp2 = System.IO.File.ReadAllBytes(filePath);
+            System.IO.File.Delete(filePath);
+            return temp2;
+        }
+    }
+
+    
+}
