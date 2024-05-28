55a89ddb10090e94e7cd4ea72c1ba69baf4114a6
FrederikkeL <fretzo@hotmail.com>
2024-05-28 16:58:23 +0200

    Ændret på projekt strukturen så tests virker

--- Blomsterbinderiet.test/UnitTest1.cs
+++ Blomsterbinderiet.test/UnitTest1.cs
@@ -1,12 +1,52 @@
+using Blomsterbinderiet.Models;
+using Blomsterbinderiet.Service;
+
 namespace Blomsterbinderiet.test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
 
+        DbGenericService<User> dbGeneric = new DbGenericService<User>();
+        UserService service = new UserService(new DbGenericService<User>());
+        User User;
+
+
+        [TestInitialize]
+        public void Initialize()
         {
+            User = new User("TestUser", "123", "Customer", "unittest@gmail.com", "12345678", "Test 1, 1000 testby");
+            service.AddUserAsync(User);
+
+        }
+
+
+        [TestMethod]
+        public async Task DisableUserTest()
+        {
+
+
+            // Arrange
+
+            List<User> testusers = service.GetAllUsersAsync().Result.Where(user => user.Email == User.Email).ToList();
+            User testUser = testusers.Single();
+            
+            
+            // Act
+            await service.DeactivateUserAsync(testUser.ID);
+
+
+            // Assert
+            User user = await service.GetUserByIdAsync(testUser.ID);
+            Assert.AreEqual(user.State, "Deaktiveret");
+            Assert.AreEqual(testUser.Name, user.Name);
+            Assert.AreEqual(testUser.Email, user.Email);
+
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            dbGeneric.DeleteObjectAsync(User);
         }
     }
 }
